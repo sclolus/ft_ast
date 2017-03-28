0f36eb0ea139c553f873f331282ccb86a8bf128b
Alexis ALVES <aalves@e2r11p5.42.fr>
2017-03-28 09:01:48 +0200

    free

--- ast/ft_free_parser.c
+++ ast/ft_free_parser.c
@@ -3,88 +3,93 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_parser.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sclolus <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: sclolus <sclolus@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/27 09:49:33 by sclolus           #+#    #+#             */
-/*   Updated: 2017/03/27 22:37:05 by sclolus          ###   ########.fr       */
+/*   Updated: 2017/03/28 07:46:09 by aalves           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
 
+void	ft_free_parser_struct(t_parser *parser)
+{
+	if (!parser)
+		return ;
+	if (parser->name)
+		free(parser->name);
+	if (parser->alloc == ALONE)
+		free(parser);
+}
+
 void	ft_free_parser_onechar(t_parser *parser)
 {
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_ref(t_parser *parser)
 {
 	free(parser->parser.ref.rule_name);
-	if (parser)
-; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_str_any_of(t_parser *parser)
 {
 	free(parser->parser.str_any_of.charset);
 	free(parser->parser.str_any_of.str);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_any(t_parser *parser)
 {
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_str_any(t_parser *parser)
 {
 	free(parser->parser.str_any.str);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_str(t_parser *parser)
 {
 	free(parser->parser.string.str);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_regexp(t_parser *parser)
 {
 	free(parser->parser.regexp.regexp);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_and_n(t_parser *parser)
 {
 	uint32_t	i;
-	
+
 	i = 0;
 	while (i < parser->parser.and.n)
 	{
 		ft_free_parser(parser->parser.and.parsers[i]);
 		i++;
 	}
-	free(parser->parser.and.parsers);
+	if (parser->retained)
+		free(parser->parser.and.parsers);
 	free(parser);
 }
 
 void	ft_free_parser_or_n(t_parser *parser)
 {
 	uint32_t	i;
-	
+
 	i = 0;
 	while (i < parser->parser.or.n)
 	{
 		ft_free_parser(parser->parser.or.parsers[i]);
 		i++;
 	}
-	free(parser->parser.or.parsers);
+	if (parser->retained)
+		free(parser->parser.or.parsers);
 	ft_put_parser_tree(parser);
 	free(parser);
 }
@@ -92,7 +97,7 @@ void	ft_free_parser_or_n(t_parser *parser)
 void	ft_free_parser_plus(t_parser *parser)
 {
 	uint32_t	i;
-	
+
 	i = 0;
 	while (i < parser->parser.plus.n)
 	{
@@ -100,14 +105,13 @@ void	ft_free_parser_plus(t_parser *parser)
 		i++;
 	}
 	ft_free_parser(parser->parser.plus.parser);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_multiply(t_parser *parser)
 {
 	uint32_t	i;
-	
+
 	i = 0;
 	while (i < parser->parser.multiply.n)
 	{
@@ -115,54 +119,46 @@ void	ft_free_parser_multiply(t_parser *parser)
 		i++;
 	}
 	ft_free_parser(parser->parser.multiply.parser);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_char_range(t_parser *parser)
 {
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_satisfy(t_parser *parser)
 {
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_satisfy_str(t_parser *parser)
 {
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_oneof(t_parser *parser)
 {
 	free(parser->parser.oneof.charset);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_not(t_parser *parser)
 {
 	free(parser->parser.not.parser);
-	if (parser)
-		; //free(parser);
-}	
+	ft_free_parser_struct(parser);
+}
 
 void	ft_free_parser_func(t_parser *parser)
 {
 	ft_free_parser(parser->parser.func.parser);
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser_undefined(t_parser *parser)
 {
 	ft_putstr("feels bad man");
-	if (parser)
-		; //free(parser);
+	ft_free_parser_struct(parser);
 }
 
 void	ft_free_parser(t_parser *parser)
@@ -186,5 +182,6 @@ void	ft_free_parser(t_parser *parser)
 		{ft_free_parser_not},
 		{ft_free_parser_plus},
 		{ft_free_parser_multiply}};
+
 	free_parsers[parser->id].f(parser);
 }
